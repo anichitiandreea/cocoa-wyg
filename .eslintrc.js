69f6fd3f181e545aaf5098969eec56fde1cab7f3
Andreea Anichiti <andreea.anichiti@gmail.com>
2023-01-27 22:08:08 +0200

    Format .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"parser": "@babel-eslint-parser",
-	"extends": "eslint:recommended",
-	"parserOptions": {
-		"ecmaVersion": 12,
-		"sourceType": "module"
-	},
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"windows"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "parser": "@babel-eslint-parser",
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      "tab"
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
 };
